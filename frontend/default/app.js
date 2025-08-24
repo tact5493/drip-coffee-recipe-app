14639cdda358d97509e900862fed73879a907624
tact5493 <takuto.505521@gmail.com>
2025-08-24 12:09:45 +0900

    [UPDATE] data/recipe.json -> ADD beans_amount

--- frontend/default/app.js
+++ frontend/default/app.js
@@ -35,44 +35,22 @@ document.getElementById("recipe-form").addEventListener("submit", async (e) => {
 
     // ステップ一覧
     const ul = document.createElement("ul");    
-    let totalAmount = 0;
+    let totalWaterAmount = 0;
     let totalTime = 0;
     let totalTimeMin = 0;
     let totalTimeSec = 0;
 
     recipe.steps.forEach((step) => {
         const li = document.createElement("li");
-        totalAmount += Number(step.water);
+        totalWaterAmount += Number(step.water);
         totalTime += Number(step.duration_sec);
         totalTimeMin = Math.floor(totalTime / 60);
         totalTimeSec = totalTime % 60;
-        li.innerHTML = `<span class="step-label">Step${step.step}:</span><br />Water: input ${step.water}ml (total ${totalAmount}ml)<br />Time: ${totalTimeMin}m ${totalTimeSec}s`;
+        li.innerHTML = `<span class="step-label">Step${step.step}:</span><br />Water: input ${step.water}ml (total ${totalWaterAmount}ml)<br />Time: ${totalTimeMin}m ${totalTimeSec}s`;
         ul.appendChild(li);
     });
     resultDiv.appendChild(ul);
 
-    // async function showSteps(steps) {
-    //   for (let i = 0; i < steps.length; i++) {
-    //     const step = steps[i];
-    //     totalAmount += Number(step.water);
-    //     totalTime += Number(step.duration_sec);
-    //     const totalTimeMin = Math.floor(totalTime / 60);
-    //     const totalTimeSec = totalTime % 60;
-
-    //     // liタグ生成
-    //     const li = document.createElement("li");
-    //     li.innerHTML = `<span class="step-label">Step${step.step}:</span><br />Water input-${step.water}ml, total-${totalAmount}ml<br />Time ${totalTimeMin}m ${totalTimeSec}s`;
-    //     ul.appendChild(li);
-
-    //     // Step1は即時表示、2ステップ目以降は前ステップのduration_sec秒待機
-    //     if (i < steps.length - 1) {
-    //       await new Promise(resolve => setTimeout(resolve, step.duration_sec * 1000));
-    //     }
-    //   }
-    // }
-
-    // // ステップ表示開始
-    // await showSteps(recipe.steps);
     
     
   } catch (error) {
